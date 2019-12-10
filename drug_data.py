0933262d411faedc66b4e9147319bdb1c19ffa36
adabi <amjad.dabi@gmail.com>
2019-12-10 16:40:05 -0600

    more data cleaning and formatting

--- drug_data.py
+++ drug_data.py
@@ -1,53 +1,76 @@
 import pubchempy as pcp
 import pandas as pd
-from padelpy import padeldescriptor
+from padelpy import padeldescriptor, from_smiles
+import numpy as np
 
-drugs_df = pd.read_csv('Data/Raw/Drugs.csv')
-drugs = list(drugs_df['drug_id'])
-smiles_lst = []
 
-# This list keeps track of the drugs for which a smiles was extracted successfully
-drugs_with_smiles = []
+def find_smiles(drugs_file):
+    drugs_df = pd.read_csv(drugs_file)
+    drugs = list(drugs_df['drug_id'])
+    smiles_lst = []
 
-# Find the isomeric smiles for all the drugs
-# for drug in drugs:
-#
-#     # Sometimes multiple Pubchem ids are listed and separated by a comma, just go with the first
-#     pubchem_id = drugs_df['pubchem'][drugs_df['drug_id'] == drug].tolist()[0].split(',')[0]
-#
-#     try:
-#         # If the Pubchem id is an integer, use that
-#         pubchem_id = int(pubchem_id)
-#         smiles = pcp.Compound.from_cid(pubchem_id).isomeric_smiles
-#         smiles_lst.append(smiles)
-#         drugs_with_smiles.append(drug)
-#
-#     except ValueError:
-#         # If it's something else like '-' or 'several' use the drug name to find the smiles
-#         drug_name = drugs_df['drug_name'][drugs_df['drug_id'] == drug].tolist()[0]
-#
-#         # get_compounds returns a list of compounds, grab the first one
-#         compound = pcp.get_compounds(drug_name, 'name')
-#
-#         if compound:
-#             smiles = compound[0].isomeric_smiles
-#             smiles_lst.append(smiles)
-#             drugs_with_smiles.append(drug)
-#
-# with open('smiles.smi', 'w+') as file:
-#     for item in smiles_lst:
-#         file.write(f'{item}\n')
-#
-# with open('drugs_with_smiles.csv', 'w+') as file:
-#     for drug in drugs_with_smiles:
-#         file.write(f'{drug}\n')
+    # This list keeps track of the drugs for which a smiles was extracted successfully
+    drugs_with_smiles = []
 
-padeldescriptor(
-                mol_dir='smiles.smi',
-                d_file='descriptors.csv',
-                convert3d=True,
-                retain3d=True,
-                d_2d=True,
-                d_3d=True,
-                fingerprints=False,
-            )
+    # Find the isomeric smiles for all the drugs
+    for drug in drugs:
+
+        # Sometimes multiple Pubchem ids are listed and separated by a comma, just go with the first
+        pubchem_id = drugs_df['pubchem'][drugs_df['drug_id'] == drug].tolist()[0].split(',')[0]
+
+        try:
+            # If the Pubchem id is an integer, use that
+            pubchem_id = int(pubchem_id)
+            smiles = pcp.Compound.from_cid(pubchem_id).isomeric_smiles
+            smiles_lst.append(smiles)
+            drugs_with_smiles.append(drug)
+
+        except ValueError:
+            # If it's something else like '-' or 'several' use the drug name to find the smiles
+            drug_name = drugs_df['drug_name'][drugs_df['drug_id'] == drug].tolist()[0]
+
+            # get_compounds returns a list of compounds, grab the first one
+            compound = pcp.get_compounds(drug_name, 'name')[0]
+
+            if compound:
+                smiles = compound.isomeric_smiles
+                smiles_lst.append(smiles)
+                drugs_with_smiles.append(drug)
+
+    with open('smiles.smi', 'w+') as file:
+        for item in smiles_lst:
+            file.write(f'{item}\n')
+
+    with open('./Data/Clean/drugs_with_smiles.csv', 'w+') as file:
+        for drug in drugs_with_smiles:
+            file.write(f'{drug}\n')
+
+
+def calculate_descriptors(drugs_file):
+    find_smiles(drugs_file)
+    # Find the descriptors from the smiles and store it
+    padeldescriptor(
+                    mol_dir='smiles.smi',
+                    d_file='./Data/Clean/descriptors.csv',
+                    convert3d=True,
+                    retain3d=True,
+                    d_2d=True,
+                    d_3d=True,
+                    fingerprints=False)
+
+    # The descriptors are sometimes out of order, sort them so they match the drugs_with_smiles order
+    descriptors_df = pd.read_csv('./Data/Clean/descriptors.csv')
+    descriptors_df['Index'] = [int(x[2]) for x in descriptors_df.Name.str.split("_")]
+    descriptors_df.set_index('Index', drop=True, inplace=True)
+    descriptors_df.sort_index(inplace=True)
+
+    # Write the correct order back to the file
+    descriptors_df.to_csv('./Data/Clean/descriptors.csv', index=False)
+
+    # Replace all the missing features and infinities with 0s (might have to change this later if
+    # the model doesn't shake out)
+    cols = (list(descriptors_df.columns))[1:]
+    descriptors_df[cols] = descriptors_df[cols].replace({np.nan: 0, 'infinity': 0})
+    descriptors_df.to_csv('./Data/Clean/descriptors_replaced.csv')
+
+
--- format_data.py
+++ format_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from collections import Counter
-import sys
+from drug_data import calculate_descriptors
+
 
 def clean_genetic_features():
     # The csv file is strangely formatted, so we need to clean it up for use with pandas
@@ -11,28 +12,27 @@ def clean_genetic_features():
             if len(line_splt) > 9:
 
                 # This combines the genes_in_segment into a single dash-delimited column
-                # instead of that variable column number monstrostiy
+                # instead of that variable column number monstrosity
                 modified_line = (line_splt[0:8] + [' - '.join(line_splt[8:])])
                 buff.append(','.join(modified_line))
             else:
                 buff.append(','.join(line_splt))
 
-        with open("Data/Raw/genetic_features_clean.csv", 'w+') as write_file:
+        with open("Data/Clean/genetic_features_clean.csv", 'w+') as write_file:
 
             write_file.write(''.join(map(str, buff)))
 
 
-genetic_featres_df = pd.read_csv('Data/Raw/genetic_features_clean.csv',
-                                 dtype={'recurrent_gain_loss': str, 'genes_in_segment': str})
-cancer_genes_df = pd.read_csv('Data/Raw/cancer_genes.csv')
-
 # This function finds mutations which show up as either sequence variation or number variation
 # and stores them in cancer_genes_relevant
-
 def find_useful_mutations():
 
+    genetic_features_df = pd.read_csv('Data/Clean/genetic_features_clean.csv',
+                                      dtype={'recurrent_gain_loss': str, 'genes_in_segment': str})
+    cancer_genes_df = pd.read_csv('Data/Raw/cancer_genes.csv')
+
     seqvar_count_lst = []
-    seqvar_count_dict = Counter(genetic_featres_df['genetic_feature'])
+    seqvar_count_dict = Counter(genetic_features_df['genetic_feature'])
 
     for gene in cancer_genes_df['gene_symbol']:
         mutations = 0
@@ -42,7 +42,7 @@ def find_useful_mutations():
 
     numb_count_lst = []
     numb_count_dict = {}
-    for row in genetic_featres_df['genes_in_segment']:
+    for row in genetic_features_df['genes_in_segment']:
         try:
             lst = [x.strip() for x in row.split('-')]
             for item in lst:
@@ -53,20 +53,22 @@ def find_useful_mutations():
         except:
             pass
 
-
-
-
     for gene in cancer_genes_df['gene_symbol']:
         try:
             numb_count_lst.append(numb_count_dict[gene])
-        except:
+        except KeyError:
             numb_count_lst.append(0)
 
     cancer_genes_df.insert(2, "count_seq", seqvar_count_lst)
     cancer_genes_df.insert(3, "count_numb", numb_count_lst)
-    cancer_genes_df.to_csv('Data/gen_feat.csv')
+
+    # The relevant genes are genes that show up in either the sequence or the
+    # copy number mutations, drop the genes that show up in neither
     cancer_genes_relevant_df = cancer_genes_df[(cancer_genes_df['count_seq'] > 0) |
                                                (cancer_genes_df['count_numb'] > 0)]
     cancer_genes_relevant_df.to_csv('Data/cancer_genes_relevant.csv')
 
-find_useful_mutations()
+#calculate_descriptors('.Data/Raw/Drugs.csv')
+#clean_genetic_features()
+#find_useful_mutations()
+
