08838b0d4bb8cfa4104b5dfe79b8c33976578438
Amjad Dabi <amjad.dabi@gmail.com>
2019-12-13 06:49:48 -0600

    Consolidated genetic features for cells

--- format_data.py
+++ format_data.py
@@ -74,5 +74,25 @@ def find_useful_mutations():
 #clean_genetic_features()
 #find_useful_mutations()
 #calculate_drug_data('./Data/Raw/Drugs.csv')
+genetic_features_df = pd.read_csv('./Data/clean/genetic_features_clean.csv')
+genetic_features_dict = {x:[] for x in pd.unique(genetic_features_df['genetic_feature'])}
+genetic_features_dict['cosmic_sample_id'] = []
+cosmic_sample_id_lst = pd.unique(genetic_features_df['cosmic_sample_id'])
+for sample in cosmic_sample_id_lst:
+    test_df = genetic_features_df[(genetic_features_df['cosmic_sample_id'] == sample) &
+                                  (genetic_features_df['is_mutated'] == 1)]
+    mutations_lst = test_df['genetic_feature'].tolist()
+    genetic_features_dict['cosmic_sample_id'].append(sample)
+    for key in genetic_features_dict:
+        if key == 'cosmic_sample_id':
+            pass
+        elif key in mutations_lst:
+            genetic_features_dict[key].append(1)
+        else:
+            genetic_features_dict[key].append(0)
+
+
+to_save = pd.DataFrame(data=genetic_features_dict)
+to_save.to_csv("./Data/Clean/sample_id_features.csv", index=False)
 
 
